8e6ff786cce1e26afad8907882002011a3516186
Praize-Jha18 <99440464+Praize-Jha18@users.noreply.github.com>
2023-05-10 16:17:59 +0100

    First commit for the files

---
+++ js/bio.js
@@ -0,0 +1,12 @@
+function openDiv() {
+    window.location.href = '../biography.html#Career';
+    
+}
+
+function openDiv2() {
+    window.location.href = '../biography.html#am';
+}
+// function color {
+//     var car document.getElementById("career");
+
+// }
